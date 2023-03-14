f8d7e736ecb5beb33b7719b2da897cf59e25ca08
Shonsu <shonsu82@gmail.com>
2023-03-14 15:33:15 +0100

    test access to component from another module

--- application/src/main/java/pl/shonsu/controller/WebTest.java
+++ application/src/main/java/pl/shonsu/controller/WebTest.java
@@ -2,12 +2,22 @@ package pl.shonsu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.shonsu.library.service.TestService;
 
 @RestController
 public class WebTest {
 
+    private final TestService testService;
+
+    public WebTest(TestService testService) {
+        this.testService = testService;
+    }
+
     @GetMapping("/")
     public String getWebTest(){
+
+        testService.testMethod();
+
         return "WEB TEST";
     }
 }
---
+++ library/src/main/java/pl/shonsu/library/service/TestService.java
@@ -0,0 +1,12 @@
+package pl.shonsu.library.service;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TestService {
+
+    public void testMethod(){
+        System.out.println("TEST");
+    }
+}
